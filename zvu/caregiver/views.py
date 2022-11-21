1342f87b8c0bf893967d9337187bd2917938f519
fbucko <filipbucko369@gmail.com>
2022-11-21 14:52:34 +0100

    Render forbidden view when user is not authorized

--- zvu/caregiver/views.py
+++ zvu/caregiver/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required, permission_required, user_passes_test
+from django.core.exceptions import PermissionDenied
+from django.http import HttpResponseForbidden
 
 # Create your views here.
-@login_required
-@user_passes_test(lambda u: u.groups.filter(name='caregiver').exists(),login_url="login")
+@login_required(login_url="login")
+#@user_passes_test(lambda u: u.groups.filter(name='caregiver').exists(), login_url='../shelter/forbidden.html')
+# @permission_required(perm='shelter.view_animal', raise_exception=True)
 def caregiver_view(request):
-    # queryset = Caregiver.objects.all() # list of objects
-    # context = {
-    #     "object_list": queryset
-    # }
-    return render(request, "caregiver/caregiver.html", {})
+    user = request.user
+    if user.groups.filter(name='caregiver').exists():
+        #exists
+        return render(request, "caregiver/caregiver.html", {})
+    # raise PermissionDenied
+    return HttpResponseForbidden()
+    
