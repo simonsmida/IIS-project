06b23bf740cbae39fe7894a33caa0ad8107448ce
Simon Smida <simon.smida@stud.fit.vutbr.cz>
2022-11-23 00:25:31 +0100

    adding new url paths

--- zvu/shelter/urls.py
+++ zvu/shelter/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('home', views.home, name='home'),
-    path('sign-up', views.sign_up, name='sign_up')
+    path('login', views.login_view, name='login'),
+    path('logout', views.logout_view, name='logout'),
+    path('sign-up', views.sign_up_view, name='sign-up'),
 ]
