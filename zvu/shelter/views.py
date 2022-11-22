c54cc0f9c0e203e5438e58a85d76912a8d9a04e3
Simon Smida <simon.smida@stud.fit.vutbr.cz>
2022-11-23 00:27:21 +0100

    updating the views

--- zvu/shelter/views.py
+++ zvu/shelter/views.py
@@ -1,65 +1,64 @@
 from django.shortcuts import render, redirect
-from .forms import RegisterForm
+from .forms import RegisterForm, LoginForm
+
 from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.models import User, Group
 
 
-def home(request):
-    # posts = Post.objects.all()
-
-    # if request.method == "POST":
-    #     post_id = request.POST.get("post-id")
-    #     user_id = request.POST.get("user-id")
-
-    #     if post_id:
-    #         post = Post.objects.filter(id=post_id).first()
-    #         if post and (post.author == request.user or request.user.has_perm("main.delete_post")):
-    #             post.delete()
-    #     elif user_id:
-    #         user = User.objects.filter(id=user_id).first()
-    #         if user and request.user.is_staff:
-    #             try:
-    #                 group = Group.objects.get(name='default')
-    #                 group.user_set.remove(user)
-    #             except:
-    #                 pass
-
-    #             try:
-    #                 group = Group.objects.get(name='mod')
-    #                 group.user_set.remove(user)
-    #             except:
-    #                 pass
-
-    return render(request, 'shelter/home.html', {})
-
-
-# @login_required(login_url="/login")
-# @permission_required("main.add_post", login_url="/login", raise_exception=True)
-# def create_post(request):
-#     if request.method == 'POST':
-#         form = PostForm(request.POST)
-#         if form.is_valid():
-#             post = form.save(commit=False)
-#             post.author = request.user
-#             post.save()
-#             return redirect("/home")
-#     else:
-#         form = PostForm()
-
-#     return render(request, 'main/create_post.html', {"form": form})
-
-
-def sign_up(request):
-    if request.method == 'POST':
-        form = RegisterForm(request.POST)
+def login_view(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('/home')
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                return render(request, 'registration/login.html', {'error': 'Username or password is incorrect'})
     else:
-        form = RegisterForm()
+        form = LoginForm()
+
     context = {
         'form': form
     }
+
+    return render(request, 'registration/login.html', context)
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
+
+
+def sign_up_view(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password')
+            confirm_password = form.cleaned_data.get('confirm_password')
+
+            if password != confirm_password:
+                return render(request, 'registration/sign_up.html', {'error': 'Passwords do not match'})
+
+            user = User.objects.create_user(username, email, password)
+            user.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = RegisterForm()
+
+    context = {
+        'form': form
+    }
+
     return render(request, 'registration/sign_up.html', context)
+
+
+def home(request):
+    return render(request, 'shelter/home.html', {})
