ee2bc6b0261690457d202d2a1ad96d65708afff1
Simon Smida <simon.smida@stud.fit.vutbr.cz>
2022-11-23 00:24:54 +0100

    updating forms - login, register

--- zvu/shelter/forms.py
+++ zvu/shelter/forms.py
@@ -2,10 +2,28 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-class RegisterForm(UserCreationForm):
+
+class RegisterForm(forms.ModelForm):
     email = forms.EmailField(required=True)
+    password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
 
     class Meta:
         model = User
-        fields = ["username", "email", "password1", "password2"]
+        fields = ['username', 'email', 'password', 'confirm_password']
+
+    # def clean(self):
+    #     cleaned_data = super(RegisterForm, self).clean()
+    #     password = cleaned_data.get('password')
+    #     confirm_password = cleaned_data.get('confirm_password')
+
+    #     if password != confirm_password:
+    #         raise forms.ValidationError(
+    #             "password and confirm_password does not match"
+    #         )
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
 
