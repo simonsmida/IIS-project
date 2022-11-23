7e7b5f46d3c4c724ea0cbb46617062996efa7a07
fbucko <filipbucko369@gmail.com>
2022-11-23 01:35:21 +0100

    Caregiver portal
    Manage volunteers in progress

---
+++ zvu/caregiver/decorators.py
@@ -0,0 +1,25 @@
+from django.http import HttpResponseForbidden
+
+def user_group(*args):
+    """
+    Decorator for checking given user groups as input.
+    Decorator runs a function only if user has one of the specified 
+    user groups. User groups are passed as a decorator parameter
+    """
+    def decorator(func):
+        def wrapper(request):
+            if request.user != None:
+                user = request.user
+                has_valid_group = False
+                for arg in args:
+                    if user.groups.filter(name=arg).exists():
+                        has_valid_group = True
+                if has_valid_group:
+                    value = func(request)
+                    return value
+                else:
+                    return HttpResponseForbidden()
+            else:
+                return HttpResponseForbidden()
+        return wrapper
+    return decorator
--- zvu/caregiver/urls.py
+++ zvu/caregiver/urls.py
@@ -1,10 +1,23 @@
 from django.urls import path
 
-from .views import caregiver_view
+from .views import (
+    caregiver_view,
+    manage_volunteers_view,
+    edit_animals_view,
+    create_schedules_view,
+    register_walks_view,
+    create_vet_request_view
+)
 
 app_name = 'caregiver'
 urlpatterns = [
-    path('', caregiver_view, name='caregiver'),
+    path('', manage_volunteers_view, name='caregiver'),
+    path('manage_volunteers/', manage_volunteers_view, name='manage-volunteers'),
+    path('edit_animals/', edit_animals_view, name='edit-animals'),
+    path('create_schedules/', create_schedules_view, name='create-schedules'),
+    path('register_walks/', register_walks_view, name='register-walks'),
+    path('create_vet_request/', create_vet_request_view, name='create-vet-request'),
+    
     # path('create/', caregiver_create_view, name='caregiver-list'),
     # path('<int:id>/', caregiver_detail_view, name='caregiver-detail'),
     # path('<int:id>/update/', caregiver_update_view, name='caregiver-update'),
