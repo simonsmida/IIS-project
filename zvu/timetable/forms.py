9630c8164341dc384f6a0e366db937b76f7dcb42
Simon Smida <simon.smida@stud.fit.vutbr.cz>
2022-11-23 23:53:25 +0100

    timetable forms

--- zvu/timetable/forms.py
+++ zvu/timetable/forms.py
@@ -1,25 +1,19 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from timetable.models import Timetable
 
 
-class TimetableForm(UserCreationForm):
+class TimetableForm(forms.ModelForm):
+    day = forms.CharField(max_length=255)
+    time_from = forms.TimeField()
+    time_to = forms.TimeField()
+    is_free = forms.PositiveIntegerField()
 
-    
-    
     class Meta:
-        model = User
-        fields = ["username", "first_name", "last_name", "password1", "password2"]
-
-# class CaregiverForm(forms.ModelForm):
-#     meno            = forms.CharField()
-#     priezvisko      = forms.CharField()
-#     datum_narodenia = forms.DateField()
-    
-#     class Meta:
-#         model = Caregiver
-#         fields = [
-#             'meno',
-#             'priezvisko',
-#             'datum_narodenia'
-#         ]
+        model = Timetable
+        fields = [
+            'day',
+            'time_from',
+            'time_to',
+            'is_free'
+        ]
