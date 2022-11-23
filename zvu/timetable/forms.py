0c5440a3de7039e502396cd7807890db7f6447e1
DanoCH-boop <danochudy@DESKTOP-379DKVR.localdomain>
2022-11-24 00:59:45 +0100

    Fix bug

--- zvu/timetable/forms.py
+++ zvu/timetable/forms.py
@@ -1,13 +1,13 @@
 from django import forms
 from django.contrib.auth.models import User
-from timetable.models import Timetable
+from shelter.models import Timetable
 
 
 class TimetableForm(forms.ModelForm):
     day = forms.CharField(max_length=255)
     time_from = forms.TimeField()
     time_to = forms.TimeField()
-    is_free = forms.PositiveIntegerField()
+    is_free = forms.IntegerField()
 
     class Meta:
         model = Timetable
