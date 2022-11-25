06f3acb4c52251a419cc20d2c238d1990f56346d
Simon Smida <simon.smida@stud.fit.vutbr.cz>
2022-11-25 16:35:35 +0100

    vetrequest forms datepicker update

--- zvu/vetrequest/forms.py
+++ zvu/vetrequest/forms.py
@@ -42,13 +42,14 @@ class MyModelChoiceField(forms.ModelChoiceField):
 #             'reserved_to',
 #             'animalid'
 #         ]
-            
+   
     
 class VetrequestForm(forms.ModelForm):
-    creation_date = forms.DateField()
+    creation_date = forms.DateField(widget=forms.widgets.DateInput(attrs={'type': 'date'}))
     content = forms.CharField(widget=forms.Textarea(attrs={"cols": "70", "rows": "8"}))
     # state = forms.ChoiceField(choices=[('pending','pending'),
     #                                   ('finished','finished')])
+
     animalid = MyModelChoiceField(
         queryset=Animal.objects.all(),
         label='Animal',
@@ -70,8 +71,9 @@ class VetrequestForm(forms.ModelForm):
             'vetid'
         ]
 
+
 class VetrequestNewForm(forms.ModelForm):
-    creation_date = forms.DateField()
+    creation_date = forms.DateField(widget=forms.widgets.DateInput(attrs={'type': 'date'}))
     content = forms.CharField(widget=forms.Textarea(attrs={"cols": "70", "rows": "8"}))
     # state = forms.ChoiceField(choices=[('pending','pending'),
     #                                   ('finished','finished')])
@@ -95,6 +97,7 @@ class VetrequestNewForm(forms.ModelForm):
             'animalid',
             'vetid'
         ]
+
         
         
 class VetrequestExamForm(forms.ModelForm):
